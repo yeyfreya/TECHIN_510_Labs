6285e7aa2cbdf038ec35eb260d87d2633c99dcd4
SHADOW <littleshadow@Shadow-the-little.local>
2024-01-09 16:30:15 -0800

    Lab1 website file

---
+++ app.py
@@ -0,0 +1,20 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+st.title ("Freya's Personal Website")
+
+st.header("About Me")
+st.write("Hello, welcome to my personal website!")
+st.write("I am Freya, a student at the University of Washington, MSTI program.")
+st.write("I am currently learning about design, programming, and entrepreneurship.")
+st.write("I love nature exploring, yoga, drawing, and cooking.")
+
+st.header("My Favorite Quote")
+st.caption("“Never stop looking up” by NASA")
+
+st.header("Contact Me")
+st.write("Feel free to contact me!")
+st.write("Email: yeyfreya@uw.edu")
+st.image('Freya.jpeg', caption='Freya by the sea in JEJU Island, Summer 2023')
+
